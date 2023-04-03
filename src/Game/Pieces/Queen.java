79d68af3ac5f7e90912163beafe2ac6f17bff6c7
Yves Kloeckner (Student) <yves.k256@slcstudents.org>
2023-04-03 14:22:49 -0600

    queen movement ready for testing

--- src/Game/Pieces/Queen.java
+++ src/Game/Pieces/Queen.java
@@ -12,8 +12,45 @@ public class Queen extends Piece {
 
 	@Override
 	public boolean isLegal(Board board, Cell s, Cell e) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'isLegal'");
+		// METHOD BEING WORKED ON
+		if (s.equals(e)) return false; // same cell
+		if (e.hasPiece()) {
+			if (e.getPiece().get_isWhite() == s.getPiece().get_isWhite()) return false; // friendly fire
+		}
+		if (s.getX() == e.getX()) { // horizontal move
+			if (e.getX() > s.getX()) { // move up
+				for (int i = s.getX() + 1; i < e.getX(); i++) { // loop through pieces between starting piece and ending piece
+					if (board.getCell(i, s.getY()).hasPiece()) return false;
+				}
+			} else { // move down 
+				for (int i = s.getX() - 1; i > e.getX(); i--) { // loop through pieces between starting piece and ending piece
+					if (board.getCell(i, s.getY()).hasPiece()) return false;
+				}
+			}
+		} else if (s.getY() == e.getY()) { // vertical move
+			if (e.getY() > s.getY()) { // move up
+				for (int i = s.getY() + 1; i < e.getY(); i++) { // loop through pieces between starting piece and ending piece
+					if (board.getCell(s.getX(), i).hasPiece()) return false;
+				}
+			} else { // move down 
+				for (int i = s.getY() - 1; i > e.getY(); i--) { // loop through pieces between starting piece and ending piece
+					if (board.getCell(s.getX(), i).hasPiece()) return false;
+				}
+			}
+		} else { // not straight line move 
+			if (e.getY() - e.getX() == s.getY() - s.getX()) { // right diagonal
+				for (int i = 1; i < e.getX(); i++) {
+					if (board.getCell(s.getX()+i, s.getY()+i).hasPiece()) return false;
+				}
+			}
+			if (e.getY() + e.getX() == s.getY() + s.getX()) { // left diagonal
+				for (int i = 1; i > e.getX(); i--) {
+					if (board.getCell(s.getX()-i, s.getY()-i).hasPiece()) return false;
+				}
+			}
+			return false;
+		}
+		return true;
 	}
 
 }
