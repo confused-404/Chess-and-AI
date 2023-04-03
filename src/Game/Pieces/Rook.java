c88fade9454af343c9055c75e9da0ac8fa3a7773
Yves Kloeckner (Student) <yves.k256@slcstudents.org>
2023-04-03 11:45:28 -0600

    rook movement ready for testing

--- src/Game/Pieces/Rook.java
+++ src/Game/Pieces/Rook.java
@@ -12,8 +12,35 @@ public class Rook extends Piece {
 
 	@Override
 	public boolean isLegal(Board board, Cell s, Cell e) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'isLegal'");
+		// METHOD FINISHED
+		if (s.equals(e)) return false; // same cell
+		if (e.hasPiece()) {
+			if (e.getPiece().get_isWhite() == s.getPiece().get_isWhite()) return false; // friendly fire
+		}
+		if (s.getX() == e.getX()) { // horizontal move
+			if (e.getX() > s.getX()) { // move up
+				for (int i = s.getX() + 1; i < e.getX(); i++) { // loop through pieces between starting piece and ending piece
+					if (board.getCell(i, s.getY()).hasPiece()) return false;
+				}
+			} else { // move down 
+				for (int i = s.getX() - 1; i > e.getX(); i--) { // loop through pieces between starting piece and ending piece
+					if (board.getCell(i, s.getY()).hasPiece()) return false;
+				}
+			}
+		} else if (s.getY() == e.getY()) { // vertical move
+			if (e.getY() > s.getY()) { // move up
+				for (int i = s.getY() + 1; i < e.getY(); i++) { // loop through pieces between starting piece and ending piece
+					if (board.getCell(s.getX(), i).hasPiece()) return false;
+				}
+			} else { // move down 
+				for (int i = s.getY() - 1; i > e.getY(); i--) { // loop through pieces between starting piece and ending piece
+					if (board.getCell(s.getX(), i).hasPiece()) return false;
+				}
+			}
+		} else { // not straight line move 
+			return false;
+		}
+		return true;
 	}
 
 }
